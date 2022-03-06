fix gradient direction, fix motion loss order, better tensorboard visualization

detectron2/modeling/losses/motion_loss.py
@@ -21,11 +21,14 @@ def motion_consistency_loss(coords_A_in_B, mask, R_A2B, R_B2A, t_A2B, t_B2A):
     # a row vector of 3 zeros. We see that the total rotation is R2*R1 and the t
     # total translation is R2*t1 + t2.
 
-    R2R1 = R_B2A @ R_A2B  # [B, 3, 3]
-    R2t1 = R_B2A[:, None, :, :] @ t_A2B.view(B, 3, 1, -1).permute(0, 3, 1, 2)  # [B, HxW, 3, 1]
+    R2R1 = R_A2B @ R_B2A  # [B, 3, 3]
+    R2t1 = R_A2B[:, None, :, :] @ sampled_t_B2A.view(B, 3, 1, -1).permute(0, 3, 1, 2)  # [B, HxW, 3, 1]
+    # R2R1 = R_B2A @ R_A2B  # [B, 3, 3]
+    # R2t1 = R_B2A[:, None, :, :] @ t_A2B.view(B, 3, 1, -1).permute(0, 3, 1, 2)  # [B, HxW, 3, 1]
 
     rot_unit = R2R1
-    trans_zero = R2t1[..., 0] + sampled_t_B2A.view(B, 3, -1).permute(0, 2, 1)
+    trans_zero = R2t1[..., 0] + t_A2B.view(B, 3, -1).permute(0, 2, 1)
+    # trans_zero = R2t1[..., 0] + sampled_t_B2A.view(B, 3, -1).permute(0, 2, 1)
 
     eyes = torch.eye(3, device=coords_A_in_B.device)[None, :, :].repeat(B, 1, 1)
 
@@ -46,8 +49,8 @@ def motion_consistency_loss(coords_A_in_B, mask, R_A2B, R_B2A, t_A2B, t_B2A):
 
 
 def motion_smoothness_loss_fn(motion_field, warp_around=False):
-    motion_gradients_x = gradient_x(motion_field)[:, :, :-1, :]
-    motion_gradients_y = gradient_y(motion_field)[:, :, :, :-1]
+    motion_gradients_x = gradient_x(motion_field, reversed=True)[:, :, 1:, :]
+    motion_gradients_y = gradient_y(motion_field, reversed=True)[:, :, :, 1:]
 
     return torch.sqrt(1e-24 + motion_gradients_x ** 2 + motion_gradients_y ** 2).mean()
 

detectron2/modeling/losses/smoothness_loss.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def gradient_x(image):
+def gradient_x(image, reversed=False):
     """
     Calculates the gradient of an image in the x dimension
     Parameters
@@ -14,10 +14,13 @@ def gradient_x(image):
     gradient_x : torch.Tensor [B,3,H,W-1]
         Gradient of image with respect to x
     """
-    return image[:, :, :, :-1] - image[:, :, :, 1:]
+    if reversed:
+        return image[:, :, :, 1:] - image[:, :, :, :-1]
+    else:
+        return image[:, :, :, :-1] - image[:, :, :, 1:]
 
 
-def gradient_y(image):
+def gradient_y(image, reversed=False):
     """
     Calculates the gradient of an image in the y dimension
     Parameters
@@ -30,10 +33,13 @@ def gradient_y(image):
     gradient_y : torch.Tensor [B,3,H-1,W]
         Gradient of image with respect to y
     """
-    return image[:, :, :-1, :] - image[:, :, 1:, :]
+    if reversed:
+        return image[:, :, 1:, :] - image[:, :, :-1, :]
+    else:
+        return image[:, :, :-1, :] - image[:, :, 1:, :]
 
 
-def smoothness_loss(depth, image):
+def smoothness_loss(depth, image, reversed=False):
     """
     Calculate smoothness values for inverse depths
 
@@ -58,11 +64,11 @@ def smoothness_loss(depth, image):
     mean_inv_depth = inv_depth.mean(2, True).mean(3, True)
     inv_depths_norm = inv_depth / mean_inv_depth.clamp(min=1e-6)
 
-    inv_depth_gradients_x = gradient_x(inv_depths_norm)
-    inv_depth_gradients_y = gradient_y(inv_depths_norm)
+    inv_depth_gradients_x = gradient_x(inv_depths_norm, reversed)
+    inv_depth_gradients_y = gradient_y(inv_depths_norm, reversed)
 
-    image_gradients_x = gradient_x(image)
-    image_gradients_y = gradient_y(image)
+    image_gradients_x = gradient_x(image, reversed)
+    image_gradients_y = gradient_y(image, reversed)
 
     weights_x = torch.exp(-torch.mean(torch.abs(image_gradients_x), 1, keepdim=True))
     weights_y = torch.exp(-torch.mean(torch.abs(image_gradients_y), 1, keepdim=True))